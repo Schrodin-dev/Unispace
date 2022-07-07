1408de72ab9f7a2755b7743d9d4f136a2f4dd8d4
DELAYGUES_Raphaël <raphael.delaygues@gmail.com>
2022-07-07 14:39:34 +0400

    [BACKEND]
    méthodes de register et login
    (manque à rajouter des vérifications sur les informations données en register + mail de vérification)

---
+++ backend/controllers/user.js
@@ -0,0 +1,57 @@
+const bcrypt = require('bcrypt');
+const db = require('../models/index');
+const jsonwebtoken = require('jsonwebtoken');
+
+exports.register = (req, res, next) => {
+    //vérification des données avant insertion
+    if(!req.body.email.toString().includes('@etu.umontpellier.fr')){
+        return res.status(400).json({message: 'Vous devez utiliser un email étudiant universitaire de Montpellier afin de vous inscrire.'});
+    }
+
+    bcrypt.hash(req.body.password, 10)
+        .then(async hash => {
+            const user = db.user.build({
+                emailUser: req.body.email,
+                nomUser: req.body.nom,
+                prenomUser: req.body.prenom,
+                mdpUser: hash,
+                nomGroupe: req.body.groupe
+
+            });
+
+            await user.save()
+                .then(() => res.status(201).json({message: 'Un email a été envoyé pour valider la création de votre compte.'})) //TODO: validation du compte par email avant de lui donner lde droit d'accéder au site
+                .catch(error => res.status(400).json({error}));
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({error});
+        });
+};
+
+exports.login = (req, res, next) => {
+    db.user.findOne({where: {emailUser: req.body.email}})
+        .then(user => {
+            if(!user){
+                return res.status(400).json({message: 'paire email/mot de passe incorrecte'});
+            }
+
+            bcrypt.compare(req.body.password, user.mdpUser)
+                .then(valide => {
+                    if(!valide){
+                        return res.status(400).json({message: 'paire email/mot de passe incorrecte'});
+                    }
+
+                    res.status(200).json({
+                        email: user.email,
+                        token: jsonwebtoken.sign(
+                            {userId : user.email},
+                            'tokenMagique',//TODO: remplacer le token en production par un truc bien long comme il faut :)
+                            {expiresIn: '24h'}
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({error}));
+        })
+        .catch(error => res.status(500).json({error}));
+};
