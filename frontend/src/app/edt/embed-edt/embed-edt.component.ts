cca5a6b7c81f16dff1cf261768d843c22112e29a
DELAYGUES_Raphaël <raphael.delaygues@gmail.com>
2022-08-13 11:48:01 +0400

    [FRONTEND] mise en page de l'embed edt

--- frontend/src/app/edt/embed-edt/embed-edt.component.ts
+++ frontend/src/app/edt/embed-edt/embed-edt.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {RequestsService} from "../../services/requests.service";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-embed-edt',
@@ -11,9 +12,11 @@ export class EmbedEdtComponent implements OnInit {
 
 	dateSelectionnee!: FormGroup;
 	edt: any;
+	couleurFond!: String;
+	couleurPrincipale!: String;
+	couleurTexte!: String;
 
-
-  constructor(private formBuilder: FormBuilder, private requests: RequestsService) { }
+  constructor(private formBuilder: FormBuilder, private requests: RequestsService, private authService: AuthService) { }
 
   ngOnInit(): void {
 		this.dateSelectionnee = this.formBuilder.group({
@@ -23,6 +26,16 @@ export class EmbedEdtComponent implements OnInit {
 
 		this.dateSelectionnee.valueChanges.forEach(newValue => {
 			this.edt = this.requests.getCours(newValue.date, newValue.date);
-		})
+		});
+
+		this.authService.couleurFond.subscribe(couleur => {
+			this.couleurFond = couleur;
+		});
+	  this.authService.couleurPrincipale.subscribe(couleur => {
+		  this.couleurPrincipale = couleur;
+	  });
+	  this.authService.textColor.subscribe(couleur => {
+		  this.couleurTexte = couleur;
+	  });
   }
 }
