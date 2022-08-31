f816b34124931e789df59ab926a103cb4637edb0
DELAYGUES_Raphaël <raphael.delaygues@gmail.com>
2022-08-31 12:31:06 +0200

    [FRONTEND] footer

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+	couleurFond!: String;
+	couleurTexte!: String;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+	  this.authService.couleurFond.subscribe(couleur => {this.couleurFond = couleur;});
+	  this.authService.couleurTexte.subscribe(couleur => {this.couleurTexte = couleur;});
   }
 
 }
