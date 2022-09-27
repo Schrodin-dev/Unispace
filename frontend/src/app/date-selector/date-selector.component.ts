6464e55437ace1ba501a28c37bdee4b549602f84
DELAYGUES_Raphaël <raphael.delaygues@gmail.com>
2022-09-27 15:59:58 +0200

    correction du bug des date dans le sélecteur de date

--- frontend/src/app/date-selector/date-selector.component.ts
+++ frontend/src/app/date-selector/date-selector.component.ts
@@ -31,14 +31,13 @@ export class DateSelectorComponent implements OnInit {
 		this.dateSubject.subscribe(date => {
 			if(date === this.date) return;
 			this.date = date;
-
 			if(this.dateSelectionnee !== undefined){
-				this.dateSelectionnee.patchValue({'date': this.date.toISOString().substr(0, 10)});
+				this.dateSelectionnee.patchValue({'date': this.formatDate(this.date)});
 			}
 		});
 
 		this.dateSelectionnee = this.formBuilder.group({
-			date: [this.date.toISOString().substr(0, 10)]
+			date: [this.formatDate(this.date)]
 		});
 
 		this.dateSelectionnee.valueChanges.forEach(newValue => {
@@ -58,5 +57,16 @@ export class DateSelectorComponent implements OnInit {
 	nextDay(){
 		this.date.setDate(this.date.getDate()+1);
 		this.dateSelectionnee.patchValue({'date': this.date.toISOString().substr(0, 10)});
+	}
+
+	/* code from: https://w3guides.com/tutorial/how-to-populate-date-input-in-angular-reactive-forms */
+	private formatDate(date: Date) {
+		const d = new Date(date);
+		let month = '' + (d.getMonth() + 1);
+		let day = '' + d.getDate();
+		const year = d.getFullYear();
+		if (month.length < 2) month = '0' + month;
+		if (day.length < 2) day = '0' + day;
+		return [year, month, day].join('-');
 	}
 }
