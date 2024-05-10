c4900c1ff9c3ce3088f5a127eb5e0825602106cb
DELAYGUES_Raphaël <raphael.delaygues@gmail.com>
2024-05-10 16:33:05 +0400

    Suppression du projet précédent Suppression de l'ensemble des fichiers précédents, puisque les technologies utilisées vont évoluer avec cette version du projet.

--- backend/routes/anneeUniv.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const anneeUnivCtrl = require('../controllers/anneeUniv');
-
-router.post('/ajouter', anneeUnivCtrl.creerAnneeUniv);
-router.post('/supprimer', anneeUnivCtrl.supprimerAnneeUniv);
-
-module.exports = router;
--- backend/routes/mail.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const mailCtrl = require('../controllers/mail');
-
-router.post('/annonce', mailCtrl.annonce);
-router.get('/destinataires', mailCtrl.listeDestinataires);
-
-module.exports = router;
